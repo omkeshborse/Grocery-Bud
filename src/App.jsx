6eb306cda574d7e4a897d94b83d454a6fe32e0d6
omkeshborse <bomkes25@gmail.com>
2023-08-25 14:44:04 +0530

    feat:localStorage integrated  add,delete & edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,49 @@ import { useState } from "react";
 import Form from "./form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
+
+const getLocalStorage = () => {
+  let list =JSON.parse(localStorage.getItem('list')) || []
+  return list
+};
+const setLocalStorage = (items  )=>{
+  localStorage.setItem('list' , JSON.stringify(items))
+}
 const App = () => {
- const [items ,setItems] = useState([]) 
+  
+ const [items, setItems] = useState(getLocalStorage()); 
  const addItems =(itemsName) =>{
    const newItem = {
     name : itemsName ,
     completed : false ,
     id : nanoid() 
    }
-   setItems([...items , newItem]) ;
+   const newItems = [...items, newItem];
+   setItems(newItems) ;
+   setLocalStorage(newItems);
  }
  const removeItem =(itemId) =>{
   const newItems = items.filter((item ) =>item.id !== itemId) ;
-  setItems(newItems)
+  setItems(newItems) ;
+  setLocalStorage(newItems) ;
  }
-  return <section className="section-center"> 
-    <Form addItems={addItems}/>
-    <Items items={items} removeItem={removeItem}/>
-  </section>;
+ const editItem = (itemId)=>{
+  const newItems = items.map(( item) =>{
+    if (item.id === itemId) {
+      const newItem = {...item , completed : !item.completed}
+      return newItem 
+    }
+    return item;
+  })
+  setItems(newItems);
+  setLocalStorage(newItems);
+ }
+  return (
+    <section className="section-center">
+      <Form addItems={addItems} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
+    </section>
+  );
 };
 
 export default App;
--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-const SingleItem = ({ item, removeItem }) => {
-  const [isChecked , setIsChecked ] = useState(item.completed) ;
+const SingleItem = ({ item, removeItem , editItem}) => {
+ 
   return (
     <div className="single-item">
-      <input type="checkbox" name="" id="" checked={isChecked} onChange={()=>{setIsChecked(!isChecked);}} />
-      <p style={{textDecoration : isChecked && 'line-through' , textTransform : "capitalize"}}>{item.name}</p>
+      <input type="checkbox"  checked={item.completed} onChange={()=>editItem(item.id)}  />
+      <p style={{textDecoration : item.completed && 'line-through' , textTransform : "capitalize"}}>{item.name}</p>
       <button className="btn remove-btn" type="button" onClick={()=>removeItem(item.id)}>
         delete
       </button>
